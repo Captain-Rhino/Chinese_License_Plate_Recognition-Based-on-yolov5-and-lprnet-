cbf8b0d5169fe6b5fdcd0e3fcf18302941644160
Captain_Rhino <1652136518@qq.com>
2024-04-25 17:39:52 +0800

    day_4_25

---
+++ cv2AddChineseText.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+import cv2
+# encoding:utf-8
+import base64
+import requests
+
+
+def cv2AddChineseText(img, text, position, textColor=(0, 255, 0), textSize=30):
+    if (isinstance(img, np.ndarray)):  # 判断是否OpenCV图片类型
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    # 创建一个可以在给定图像上绘图的对象
+    draw = ImageDraw.Draw(img)
+    # 字体的格式
+    fontStyle = ImageFont.truetype(
+        "simsun.ttc", textSize, encoding="utf-8")
+    # 绘制文本
+    draw.text(position, text, textColor, font=fontStyle)
+    # 转换回OpenCV格式
+    return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
